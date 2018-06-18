08f86d8900487a8ec97d024531e74070b1433999
ekno27 <edgar.canozelaya.6@my.csun.edu>
2018-06-18 10:10:43 -0700

    rearranged functions and started 3rd option

--- bamazonManager.js
+++ bamazonManager.js
@@ -18,6 +18,82 @@ var connection= mysql.createConnection({
     
 // });
 
+/*
+*function declaration
+*/
+
+//function used in order to display all the items in the database
+function viewProducts(){
+    connection.query("SELECT * FROM products", function(err,res){
+        if(err) throw err;
+        var item_id;
+        var product_name;
+        var department_name;
+        var price; 
+        var stock_quantity;
+        for(var i = 0; i<res.length;i++){
+            item_id = res[i].item_id;
+            product_name = res[i].product_name;
+            department_name = res[i].department_name;
+            price = res[i].price;
+            stock_quantity = res[i].stock_quantity;
+            
+            console.log("item ID: "+ item_id +"  Product Name: "+product_name + "\nDepartment: "+department_name+ "\nPrice: "+price+"\nStock Quantity: "+stock_quantity +"\n" );
+
+            
+        }//end of loop
+        connection.end();
+    })//end of query 
+}//end of function
+
+//function used to show the items with low inventory 
+function ViewLowInventory(){
+    connection.query("SELECT * FROM products where stock_quantity<5", function(err,res){
+        if(err) throw err;
+        var item_id;
+        var product_name;
+        var department_name;
+        var price; 
+        var stock_quantity;
+        for(var i = 0; i<res.length;i++){
+            item_id = res[i].item_id;
+            product_name = res[i].product_name;
+            department_name = res[i].department_name;
+            price = res[i].price;
+            stock_quantity = res[i].stock_quantity;
+            
+            console.log("item ID: "+ item_id +"  Product Name: "+product_name + "\nDepartment: "+department_name+ "\nPrice: $ "+price+"\nStock Quantity: "+stock_quantity +"\n" );
+
+            
+        }//end of loop
+        connection.end();
+    })//end of query
+}//end of function 
+
+
+//function used to allow client to add items to inventory database
+function addToInventory(){
+    inquirer.query([
+        {
+            type:"input",
+            message: "Enter name of product: ",
+            name: "productName"
+        },
+        {
+            type: "input",
+            message: "Enter price:",
+            name: "productPrice"
+        },
+        {
+            type:"input",
+            message: "Enter amount of the product you'd like to add: ",
+            name:"productQuantity"
+        }
+
+    ])//end of query
+    
+}
+
 //main menu
 function mainMenu(){
     inquirer.prompt([{
@@ -32,61 +108,23 @@ function mainMenu(){
         }
         switch(data.clientOption){
             case "View Products for sale":
-            connection.query("SELECT * FROM products", function(err,res){
-                if(err) throw err;
-                var item_id;
-                var product_name;
-                var department_name;
-                var price; 
-                var stock_quantity;
-                for(var i = 0; i<res.length;i++){
-                    item_id = res[i].item_id;
-                    product_name = res[i].product_name;
-                    department_name = res[i].department_name;
-                    price = res[i].price;
-                    stock_quantity = res[i].stock_quantity;
-                    
-                    console.log("item ID: "+ item_id +"  Product Name: "+product_name + "\nDepartment: "+department_name+ "\nPrice: "+price+"\nStock Quantity: "+stock_quantity +"\n" );
-
-                    
-                }//end of loop
-                connection.end();
-            })//end of query 
+            //viewProducts
+            viewProducts();
             break;
+
             //2nd case 
             case "View low inventory":
-            connection.query("SELECT * FROM products where stock_quantity<5", function(err,res){
-                if(err) throw err;
-                var item_id;
-                var product_name;
-                var department_name;
-                var price; 
-                var stock_quantity;
-                for(var i = 0; i<res.length;i++){
-                    item_id = res[i].item_id;
-                    product_name = res[i].product_name;
-                    department_name = res[i].department_name;
-                    price = res[i].price;
-                    stock_quantity = res[i].stock_quantity;
-                    
-                    console.log("item ID: "+ item_id +"  Product Name: "+product_name + "\nDepartment: "+department_name+ "\nPrice: $ "+price+"\nStock Quantity: "+stock_quantity +"\n" );
-
-                    
-                }//end of loop
-                connection.end();
-            })//end of query
+            //view low inventory 
+            ViewLowInventory();
             break;
-
-
-
             ///TODO: FINISH 3RD AND 4TH CASE
-
-
 
             //3rd option
             case "Add to inventory":
             connection.query("SELECT item_id, product_name from products")
             break;
+            
+            //4th options
             case "View low inventory":
             console.log("2nd option");
             break;
