40105b704c8edca8eb7fbaa1a62c63ea07fa4eb5
ekno27 <edgar.canozelaya.6@my.csun.edu>
2018-06-18 18:18:01 -0700

    finished bamazon manager

--- bamazonManager.js
+++ bamazonManager.js
@@ -70,29 +70,91 @@ function ViewLowInventory(){
     })//end of query
 }//end of function 
 
+//function used to add stock of an item picked 
+function addInventory(){
+    connection.query("SELECT item_id, product_name FROM products", function(err,res){
+        if(err) throw err;
+        var idOptions= [];
+        // loop retrieve each item id and name to put in the array of choices in the inquirer
+        for (var i= 0; i <res.length;i++){
+            idOptions.push("id "+ res[i].item_id+ ": "+ res[i].product_name);
+        }
+        inquirer.prompt([
+            {
+                type: "list",
+                message:"What item would you like to restock?",
+                choices: idOptions,
+                name: "restock_choiceID"
+
+            },
+            {
+                type:"input",
+                message:"How many are you adding?",
+                name: "restockAmount"
+            }
+         
+        ])//end of questions
+        .then(function(data,error){
+            var restock_choiceID = parseInt(data.restock_choiceID.match(/\d+/)[0]);
+            var restockAmount = parseInt(data.restockAmount);
+            console.log(restock_choiceID);
+            console.log(restockAmount);
+            connection.query("UPDATE products SET stock_quantity = stock_quantity + ? where item_id = ?", [restockAmount, restock_choiceID],function(err,res){
+                if (err) throw err;
+                console.log("Cool thanks for the cargo");
+                connection.end();
+            });//end of inner query
+
+
+        });//end of .then
+    })//end of connection.query
+}
+
 
 //function used to allow client to add items to inventory database
-function addToInventory(){
-    inquirer.query([
+function addNewProduct(){
+    inquirer.prompt([
         {
             type:"input",
             message: "Enter name of product: ",
-            name: "productName"
+            name: "newName"
+        },
+        {
+            type:"input",
+            message:"What department does this item belong in?",
+            name: "newDepartment"
         },
         {
             type: "input",
             message: "Enter price:",
-            name: "productPrice"
+            name: "newPrice"
         },
         {
             type:"input",
             message: "Enter amount of the product you'd like to add: ",
-            name:"productQuantity"
+            name:"newQuantity"
         }
 
     ])//end of query
-    
-}
+    .then(function(data,error){
+        if(error){
+            console.log("error: "+ error);
+        }
+       
+        var newName = data.newName;
+        var newDepartment = data.newDepartment;
+        var newPrice= parseFloat(data.newPrice);
+        newPrice= newPrice.toFixed(2);
+        var newQuantity = parseInt(data.newQuantity);
+        connection.query("INSERT INTO products (product_name, department_name, price, stock_quantity) values(?,?,?,?)",[newName,newDepartment,newPrice,newQuantity],function(err,res){
+            if(err) throw err;
+
+            console.log("Product added successfully now get out of my warehouse");
+            connection.end();
+        });//end of query
+        
+    });//end of then
+}//end of function
 
 //main menu
 function mainMenu(){
@@ -121,12 +183,14 @@ function mainMenu(){
 
             //3rd option
             case "Add to inventory":
-            connection.query("SELECT item_id, product_name from products")
+            //add inventory
+            addInventory();
             break;
             
-            //4th options
-            case "View low inventory":
-            console.log("2nd option");
+            //4th option
+            case "Add New Product":
+            //add a new product
+            addNewProduct();
             break;
 
         }
